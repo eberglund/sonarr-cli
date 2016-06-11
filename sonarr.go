d4179966519f5c7e24b5f6e29ce6342ad383c35e
Eric Berglund <ericberglund10@gmail.com>
2016-06-10 19:14:49 -0700

    Working call with api key authentication

--- sonarr.go
+++ sonarr.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+	"strings"
 )
+
+const baseUrl = "http://localhost:8989/api/"
 
 func main() {
 	refreshSeries()
 }
 
 func refreshSeries() {
-	endpoint := "http://localhost:8989/api/command"
-	data := url.Values{}
-	data.Set("name", "RefreshSeries")
-	resp, err := http.PostForm(endpoint, data)
+	apiKey := readApiKey()
+	endpoint := baseUrl + "command?apikey=" + apiKey
+	fmt.Println(endpoint)
+	data := []byte(`{"name":"RefreshSeries"}`)
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(data))
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
 
 	if err != nil {
 		fmt.Println(err)
@@ -30,4 +38,16 @@ func refreshSeries() {
 
 	fmt.Println("submitted!")
 	fmt.Printf("%s", body)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readApiKey() string {
+	key, err := ioutil.ReadFile("api_key")
+	check(err)
+	return strings.TrimSpace(string(key))
 }
